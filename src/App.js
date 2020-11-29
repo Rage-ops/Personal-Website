05459aa3fda3a7fda93584200cd39b263f94d16a
Harsha <harsha.sam23@gmail.com>
2020-11-29 15:48:06 +0530

    responsive fix -1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
 // import Blog from './Components/Blog/Blog';
-import './App.css';
+import FullHeight from "react-full-height";
 import {
   BrowserRouter as Router,
   Route,
@@ -29,23 +29,25 @@ class App extends Component{
     return (
       <Router>
         <div>
-          <Navigation navitems={["HOME", "ABOUT", "PROJECTS", "CONTACT"]} active={ this.state.currWindow }/>
-          <Route exact path="/ABOUT">
-            <About onWindowChange={ this.onWindowChange }/>
-          </Route>
-          <Route exact path="/">
-            <Home onWindowChange={ this.onWindowChange }/>
-          </Route>
-          <Route exact path="/HOME">
-            <Home onWindowChange={ this.onWindowChange }/>
-          </Route>
-          <Route exact path="/PROJECTS">
-            <Projects onWindowChange={ this.onWindowChange }/>
-          </Route>
-          {/* <Route exact path="/BLOG">
-            <Blog onWindowChange={ this.onWindowChange }/>
-          </Route> */}
-          <Footer />
+          <FullHeight>
+            <Navigation navitems={["HOME", "ABOUT", "PROJECTS", "CONTACT"]} active={ this.state.currWindow }/>
+            <Route exact path="/ABOUT">
+              <About onWindowChange={ this.onWindowChange }/>
+            </Route>
+            <Route exact path="/">
+              <Home onWindowChange={ this.onWindowChange }/>
+            </Route>
+            <Route exact path="/HOME">
+              <Home onWindowChange={ this.onWindowChange }/>
+            </Route>
+            <Route exact path="/PROJECTS">
+              <Projects onWindowChange={ this.onWindowChange }/>
+            </Route>
+            {/* <Route exact path="/BLOG">
+              <Blog onWindowChange={ this.onWindowChange }/>
+            </Route> */}
+            <Footer />
+          </FullHeight>
         </div>
       </Router>
     ); 
