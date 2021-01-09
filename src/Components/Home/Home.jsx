46bd86b506c47327e945dbae6bd94f53e176a496
Harsha <harsha.sam23@gmail.com>
2021-01-09 08:08:04 +0530

    added blogs to the home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Blog from "../Blog/Blog";
 import Button from "../Button/Button";
 import "./Home.css";
 
 const Home = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dev.to/api/articles?username=rageops")
+            .then(response => response.json())
+            .then(data => {
+                setPosts(data)
+                setIsLoading(false);
+            })
+            .catch(err => console.log(err))
+    }, [])
     return (
-        <div className="header">
-            <h1 className="header-txt">
-                Hi, I'm Harsha! <span className="wave">👋🏻 </span>
-            </h1>
-            <h2 className="header-description code">
-                A developer who's keen on learning something new everyday.
-            </h2>
-            <span className="home-btn">
-                <Button link={"About"}
-                    btnData={"Know more ↗"}
-                    btnColor={"btn-dark"}
-                />
-            </span>
-            <span className="home-btn">
-                <Button
-                    link={`mailto:harsha.sam23@gmail.com`}
-                    btnData={`Contact me`}
-                    btnColor={"btn-white"}
-                />
-            </span>
+        <div>
+            <div className="header">
+                <h1 className="header-txt">
+                    Hi, I'm Harsha! <span className="wave">👋🏻 </span>
+                </h1>
+                <h2 className="header-description code">
+                    A developer who's keen on learning something new everyday.
+                </h2>
+                <span className="home-btn">
+                    <Button link={"About"}
+                        btnData={"Know more ↗"}
+                        btnColor={"btn-dark"}
+                    />
+                </span>
+                <span className="home-btn">
+                    <Button
+                        link={`mailto:harsha.sam23@gmail.com`}
+                        btnData={`Contact me`}
+                        btnColor={"btn-white"}
+                    />
+                </span>
+            </div>
+            <Blog isLoading={isLoading} posts={posts}/>
         </div>
     );
 };
